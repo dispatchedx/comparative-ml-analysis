271c4d3393d8cbc389a1e5411be2f82ff230c5b2
dispatchedx <dispatchedx@outlook.com>
2025-10-01 06:07:53 +0300

    refactor 2025 after years

--- news_headline_classifier.py
+++ news_headline_classifier.py
@@ -5,17 +5,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import model_selection
 from sklearn import metrics
 from keras import Sequential
-from keras.layers import Dense, Flatten, Embedding, GlobalMaxPool1D, Dropout, GlobalAveragePooling1D
+from keras.layers import Dense, Embedding, GlobalAveragePooling1D
 from keras.callbacks import EarlyStopping
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-nltk.download('punkt')  # needed for nltk
-nltk.download('stopwords')  # needed for stopwords removal
+nltk.download("punkt")  # needed for nltk
+nltk.download("stopwords")  # needed for stopwords removal
 
 # Read csv into a pandas dataframe
-data = pd.read_csv('C:/Users/DX/Desktop/work/datamining/onion-or-not.csv')
+data = pd.read_csv("C:/Users/DX/Desktop/work/datamining/onion-or-not.csv")
 
 
 def preprocessing(dataframe):
@@ -26,23 +26,24 @@ def preprocessing(dataframe):
     """
 
     def pd_to_list(dataframe):
-        """ Append all titles to list and make them lower case """
+        """Append all titles to list and make them lower case"""
         document_contents = []
-        for index in range(len(dataframe['text'])):
-            document_contents.append(dataframe['text'][index].lower())
+        for index in range(len(dataframe["text"])):
+            document_contents.append(dataframe["text"][index].lower())
         return document_contents
 
     def depunctuate(list_of_strings):
-        """ Remove punctuations (except ‘) """
-        depunctuated_list = [re.sub('[!@#$%&’()*+,\\\'-./:;”“<=>?\"[\]^_—`{|}~\d…]', '', title) for title in
-                             list_of_strings]
+        """Remove punctuations (except ‘)"""
+        depunctuated_list = [
+            re.sub("[!@#$%&’()*+,\\'-./:;”“<=>?\"[\]^_—`{|}~\d…]", "", title) for title in list_of_strings
+        ]
         return depunctuated_list
 
     def discretize(list_of_strings):
-        """ Remove repeating words from each title
+        """Remove repeating words from each title
         :param list_of_strings:
         :return: list of strings
-         """
+        """
         discrete_titles = [list(set(title.split())) for title in list_of_strings]
         return discrete_titles
 
@@ -67,7 +68,7 @@ def preprocessing(dataframe):
         :param list_of_strings: list of titles
         :return: list of titles
         """
-        stop_words = set(nltk.corpus.stopwords.words('english'))
+        stop_words = set(nltk.corpus.stopwords.words("english"))
         sp_titles = []
         for title in list_of_strings:
             temp = []
@@ -76,7 +77,7 @@ def preprocessing(dataframe):
                     temp.append(word)
             sp_titles.append(temp)
         # Convert back to list of strings
-        list_of_strings = [' '.join(title) for title in sp_titles]
+        list_of_strings = [" ".join(title) for title in sp_titles]
         return list_of_strings
 
     def tf_idf(list_of_strings):
@@ -94,10 +95,10 @@ def preprocessing(dataframe):
     titles = discretize(titles)
     titles = stemming(titles)
     titles = stop_words_removal(titles)
-    #X = tf_idf(titles) # tf_idf ruins it idk why
+    # X = tf_idf(titles) # tf_idf ruins it idk why
     # will just vectorize instead for now..
 
-    tokenizer = Tokenizer(8000, split=' ')
+    tokenizer = Tokenizer(8000, split=" ")
     tokenizer.fit_on_texts(titles)
 
     X = tokenizer.texts_to_sequences(titles)
@@ -106,28 +107,27 @@ def preprocessing(dataframe):
 
 
 X = preprocessing(data)
-target = data['label'].values
+target = data["label"].values
 # Splitting training-testing data to 75%-25%
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, target, test_size=0.25, random_state=1)
 
 model = Sequential()
-model.add(Embedding(8000, 128, input_length = X.shape[1]))  # better instead of flatten for words though we have tf_idf
+model.add(Embedding(8000, 128, input_length=X.shape[1]))  # better instead of flatten for words though we have tf_idf
 
-#model.add(Dense(128, activation='relu')) # doesnt do anything
-#model.add(Flatten()) #trash and slow
+# model.add(Dense(128, activation='relu')) # doesnt do anything
+# model.add(Flatten()) #trash and slow
 
-model.add(Dense(128, activation='relu'))
-model.add(GlobalAveragePooling1D()) # pretty good
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(Dense(128, activation="relu"))
+model.add(GlobalAveragePooling1D())  # pretty good
+model.add(Dense(1, activation="sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 # cross entropy loss function is for a binary classification
 # adam because its an efficient stochastic gradient descent algorithm because it automatically tunes itself and gives good results in a wide range of problems
 model.summary()
 # Callback to stop training when val_loss stops getting reduced (to not overfit)
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
+es = EarlyStopping(monitor="val_loss", mode="min", verbose=1)
 
 history = model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test), callbacks=[es])
 predictions = model.predict(X_test)
 y_predict = [round(x[0]) for x in predictions]  # prediction is 1 if probability > 50% and 0 if its < 50%
 print(metrics.classification_report(y_predict, y_test))
-
--- wine_quality_predictor.py
+++ wine_quality_predictor.py
@@ -7,13 +7,26 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 # reading csv data
-data = pd.read_csv('C:/Users/DX/Desktop/work/datamining/winequality-red.csv')
+data = pd.read_csv("C:/Users/DX/Desktop/work/datamining/winequality-red.csv")
 
 # labels
-X = data[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
-          'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']].values
+X = data[
+    [
+        "fixed acidity",
+        "volatile acidity",
+        "citric acid",
+        "residual sugar",
+        "chlorides",
+        "free sulfur dioxide",
+        "total sulfur dioxide",
+        "density",
+        "pH",
+        "sulphates",
+        "alcohol",
+    ]
+].values
 # target
-y = data['quality'].values
+y = data["quality"].values
 
 
 def average_ph(X):
@@ -24,8 +37,8 @@ def average_ph(X):
     """
     sum = 0
     for i in range(0, len(X)):
-        sum += X[i][-3] # -3 is pH column
-    average = sum/len(X)
+        sum += X[i][-3]  # -3 is pH column
+    average = sum / len(X)
     return average
 
 
@@ -38,7 +51,7 @@ def train_svc_model(X, y):
     # Splitting training-testing data at 75%-25%
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.25, random_state=1)
     # fit SVC
-    svclassifier = SVC(kernel='linear')
+    svclassifier = SVC(kernel="linear")
     print("Training svc model..")
     svclassifier.fit(X_train, y_train)
     # Print metrics report
@@ -46,15 +59,29 @@ def train_svc_model(X, y):
     print("Training finished, results are:\n")
     print(metrics.classification_report(y_test, y_predict))
 
+
 def remove_33_of_ph_values():
     """
 
     :return: split data but yy_test is 33% of pH values and set to None
     """
-    Xx = data[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
-               'total sulfur dioxide', 'density', 'sulphates', 'alcohol', 'quality']].values
+    Xx = data[
+        [
+            "fixed acidity",
+            "volatile acidity",
+            "citric acid",
+            "residual sugar",
+            "chlorides",
+            "free sulfur dioxide",
+            "total sulfur dioxide",
+            "density",
+            "sulphates",
+            "alcohol",
+            "quality",
+        ]
+    ].values
     # target
-    yy = data['pH'].values
+    yy = data["pH"].values
 
     # Splitting training data again to have 33% of pH values set to None
     Xx_train, Xx_test, yy_train, yy_test = model_selection.train_test_split(Xx, yy, test_size=0.33, random_state=1)
@@ -64,38 +91,53 @@ def remove_33_of_ph_values():
     return Xx_train, Xx_test, yy_train, yy_test
 
 
-print("What to do?\n"
-      "1. Part A (split dataset to training-test 75%-25% and predict quality)\n"
-      "2. Part B (remove 33% of ph values and fill them with various ways)\n"
-      "Any other key. Quit\n")
+print(
+    "What to do?\n"
+    "1. Part A (split dataset to training-test 75%-25% and predict quality)\n"
+    "2. Part B (remove 33% of ph values and fill them with various ways)\n"
+    "Any other key. Quit\n"
+)
 
 choice = input()
-if choice == '1':
+if choice == "1":
     train_svc_model(X, y)
     exit(1)
-elif choice == '2':
+elif choice == "2":
 
-
-    print("What to do?\n"
-          "1. Remove pH column\n"
-          "2. Fill missing values with average pH\n"
-          "3. Fill missing values with Logistic Regression\n"
-          "4. Fill missing values with K-means cluster average\n"
-          "Any other key. Quit\n")
+    print(
+        "What to do?\n"
+        "1. Remove pH column\n"
+        "2. Fill missing values with average pH\n"
+        "3. Fill missing values with Logistic Regression\n"
+        "4. Fill missing values with K-means cluster average\n"
+        "Any other key. Quit\n"
+    )
     choice = input()
-    if choice == '1':
-        X = data[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
-                  'free sulfur dioxide', 'total sulfur dioxide', 'density', 'sulphates', 'alcohol']].values
+    if choice == "1":
+        X = data[
+            [
+                "fixed acidity",
+                "volatile acidity",
+                "citric acid",
+                "residual sugar",
+                "chlorides",
+                "free sulfur dioxide",
+                "total sulfur dioxide",
+                "density",
+                "sulphates",
+                "alcohol",
+            ]
+        ].values
         print("pH column removed")
         train_svc_model(X, y)
-    elif choice == '2':
+    elif choice == "2":
         average = average_ph(X)
         for i in range(0, len(X), 3):
             X[i][-3] = average
         print(f"33% of pH column replaced with average pH={average}")
         train_svc_model(X, y)
 
-    elif choice == '3':
+    elif choice == "3":
         Xx_train, Xx_test, yy_train, yy_test = remove_33_of_ph_values()
         # fit regression model
         regress = linear_model.LinearRegression()
@@ -107,26 +149,38 @@ elif choice == '2':
         for i in range(0, len(yy_test)):
             yy_test[i] = int(yy_predict[i])
         X = data[
-            ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
-             'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']].values
-        yy = np.concatenate([yy_train,yy_test])
+            [
+                "fixed acidity",
+                "volatile acidity",
+                "citric acid",
+                "residual sugar",
+                "chlorides",
+                "free sulfur dioxide",
+                "total sulfur dioxide",
+                "density",
+                "pH",
+                "sulphates",
+                "alcohol",
+            ]
+        ].values
+        yy = np.concatenate([yy_train, yy_test])
         for i in range(len(X)):
             X[i][-3] = yy[i]
-        y = data['quality'].values
+        y = data["quality"].values
         train_svc_model(X, y)
-    elif choice == '4':
+    elif choice == "4":
         # Removing 33% of pH values
         Xx_train, yy_train, Xx_test, yy_test = remove_33_of_ph_values()
         kmeans = KMeans(n_clusters=6).fit(X)
         labels = kmeans.predict(X)
         centroids = kmeans.cluster_centers_
         cluster_map = pd.DataFrame()
-        cluster_map['data_index'] = data.index.values
-        cluster_map['cluster'] = kmeans.labels_
+        cluster_map["data_index"] = data.index.values
+        cluster_map["cluster"] = kmeans.labels_
         print(cluster_map)
         train_svc_model(X, y)
-        #plt.scatter(data['quality'], data['pH'], c=kmeans.labels_.astype(float), s=50, alpha=0.5)
-        #plt.show()
+        # plt.scatter(data['quality'], data['pH'], c=kmeans.labels_.astype(float), s=50, alpha=0.5)
+        # plt.show()
     else:
         exit(1)
 else:
